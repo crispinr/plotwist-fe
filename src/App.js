90df19a5ec167ff81530552593bfffdaf4686521
alwinjebastine <alwinjebastine@gmail.com>
2022-05-09 20:44:22 +0530

    update in app.js and pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, 
+         Routes, 
+         Route,
+        } from "react-router-dom";
 import "./assets/scss/style.scss";
 import Landing from "./pages/Landing";
+import Feed from "./pages/Feed";
+import Login from "./pages/Login";
+import Post from "./pages/Post";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
 
-function App() {
+export default function App() {
   return (
     <>
       <Router>
-        <Routes>
-          <Route exact path="/" element={<Landing />} />
-        </Routes>
+          <Routes>
+          <Route path="/" element={<Landing/>}/>
+          <Route path="/feed" element={<Feed/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/post" element={<Post/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+          </Routes>
       </Router>
     </>
   );
 }
 
-export default App;
